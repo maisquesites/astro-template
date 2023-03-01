108c42ff6f98f4c0aaf7610a439ac0d5620905e9
Pinheiro-Lucas <lucas.gpinheiro2002@gmail.com>
2023-02-28 21:34:16 -0300

    Project Structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), prefetch(), image()]
+});
