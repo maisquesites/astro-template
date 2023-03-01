1f2f12a84cf74abebb470a550e157f628d997dfe
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-03-01 09:49:10 -0300

    Update template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
-import image from "@astrojs/image";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch(), image()]
-});
+export default defineConfig({});
